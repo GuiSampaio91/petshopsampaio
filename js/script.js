fe0b1c5ba2ec4c8f3e0483232eff5c6e6d6dbf98
GuiSampaio91 <143126300+GuiSampaio91@users.noreply.github.com>
2024-09-09 15:00:41 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Scripts para inicializar o carrossel e outras funcionalidades
+    console.log("Página carregada com sucesso.");
+});
